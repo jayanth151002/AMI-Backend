3bd5fafeb3d0ac212c04b49171ec5c23494fd5f7
VTHARUN01 <v.tharun2589@gmail.com>
2022-11-01 02:23:47 +0530

    feat: model setup done

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 dotenv.config();
 
 const app: Express = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!!");
+app.get("/", async (req: Request, res: Response) => {
+  const cam = await prisma.cameraDB.findMany({});
+  res.json(cam);
 });
 
 app.listen(port, () => {
