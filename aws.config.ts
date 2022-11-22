9d797c2ce5f3d04a10ee70220fcf99fe3db3ad2c
Jayanth <jayanth151002@gmail.com>
2022-11-22 16:21:33 +0530

    Configured AWS DynamoDB

---
+++ aws.config.ts
@@ -0,0 +1,7 @@
+const config = {
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    region: 'ap-south-1'
+}
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,41 @@
+import AWS from 'aws-sdk';
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { PrismaClient } from "@prisma/client";
-import findNearestCameras from "./utils/findNearestCameras";
-import updatedCamObj from "./types/updatedCamObj";
-
-const prisma = new PrismaClient();
+import config from "../aws.config"
 dotenv.config();
 
+AWS.config.update(config);
 const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 const port = process.env.PORT;
 
 app.get("/", async (req: Request, res: Response) => {
-  const cameraDetails = await prisma.cameraDB.findMany({});
-  const nearestCameras: updatedCamObj[] = findNearestCameras(cameraDetails);
-  res.send(nearestCameras);
-});
+  const docClient = new AWS.DynamoDB.DocumentClient();
+
+  const params = {
+    TableName: "cameraDB",
+  };
+
+  docClient.scan(params, function (err, data) {
+
+    if (err) {
+      console.log(err)
+      res.send({
+        success: false,
+        message: err
+      });
+    } else {
+      const { Items } = data;
+      console.log(Items)
+      res.send({
+        success: true,
+        movies: Items
+      });
+    }
+  });
+})
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
